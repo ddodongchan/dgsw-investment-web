d885b11eff949ab11ed187086cf7579a0f482eb0
je355 <chaeeehui@gmail.com>
2025-04-08 08:05:39 +0900

    setting :: routing... ㅠㅠ

--- src/pages/api/router.ts
+++ src/pages/api/router.ts
@@ -6,10 +6,12 @@ const useAppRouter = () => {
   return {
     goHome: () => router.push("/"),
     goToAbout: () => router.push("/main"),
+    goToArticle: () => router.push("/article"),
+    goToMyPage: () => router.push("/mypage"),
     goToProduct: (id: string) => router.push(`/product/${id}`),
     goBack: () => router.back(),
     reloadPage: () => router.reload(),
   };
 };
 
-export default useAppRouter;
+export default useAppRouter
---
+++ src/pages/mypage/index.tsx
@@ -0,0 +1,9 @@
+const Mypage = () => {
+  return (
+    <div>
+      <h1>My Page</h1>
+    </div>
+  );
+};
+
+export default Mypage;
